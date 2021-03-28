24e893121be2a1b529a2c249d364c72ac72b8dca
danmoke <975696229@qq.com>
2021-03-28 10:38:27 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # vue-jwt-admin
-开发oauth-jwt配套的后台管理
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {},
+}
+</script>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  height: 60px;
+  position: fixed;
+  right: 0;
+  left: 200px;
+}
+.el-header {
+  top: 0;
+}
+.el-footer {
+  bottom: 0;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+}
+
+.el-container > .el-main {
+  padding: 10px;
+  background-color: #e9eef3;
+  margin: 60px 0 60px 200px;
+}
+</style>
